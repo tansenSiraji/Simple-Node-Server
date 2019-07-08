57f5c8903b72077fab582a4d97208718b386b2aa
cis101 <tansenananda@gmail.com>
2019-07-08 09:45:37 +0600

    classwork8b

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var server = http.Server(app);
 
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html', function(err,data){
-      if(err){
-        return console.log('File read error');
-      }
-      res.end(data);
-    })
+  // var server = http.createServer(function(req, res){
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/html');
+  //   fs.readFile('index.html', function(err,data){
+  //     if(err){
+  //       return console.log('File read error');
+  //     }
+  //     res.end(data);
+  //   })
     
-  });
+  // });
+
+  app.get('/', function(req, res){
+    res.sendFile(__dirname+'/index.html')
+  })
+
+  app.get('/about/second', function(req, res){
+    res.sendFile(__dirname+'/second.html')
+  })
+
   server.listen(3000, "localhost", function(){
     console.log('Server running');
   });
