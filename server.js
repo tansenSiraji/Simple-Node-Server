61b2fad6ee78f8a0858dd55548ab0aa2eb943a35
cis101 <tansenananda@gmail.com>
2019-07-10 09:24:37 +0600

    classwork 8c

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.Server(app);
+var bodyParser = require('body-parser');
 
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
   // var server = http.createServer(function(req, res){
   //   res.statusCode = 200;
   //   res.setHeader('Content-Type', 'text/html');
@@ -23,6 +28,16 @@ var server = http.Server(app);
     res.sendFile(__dirname+'/second.html')
   })
 
+  app.get('/form', function(req, res){
+    res.sendFile(__dirname+'/form.html')
+  })
+
+  app.post('/article/new', function(req, res){
+    //res.sendFile(__dirname+'/form.html')
+    console.log(req.body)
+    res.send({message: "data received"})
+  })
+
   server.listen(3000, "localhost", function(){
-    console.log('Server running');
+    console.log('Server running bruh');
   });
